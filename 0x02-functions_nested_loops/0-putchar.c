701b53aa9ee025c21db5490ec295559fdd5e7ad9
esramohammednour <esraa2023112@gmail.com>
2023-09-12 09:41:36 -0700

    s

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program to print _putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
