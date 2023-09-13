9ee57843303b43125a29d6c8f2309419dd633c7c
esramohammednour <esraa2023112@gmail.com>
2023-09-13 02:16:18 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program to  Write a function to print_alphabet 
+ * Description: A C program to write a function to print_alphabet 
  *
  * Return: Always 0 (Success)
 */
