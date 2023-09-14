f599e4a93a9f1723c5a61e468a7017215a573a06
esramohammednour <esraa2023112@gmail.com>
2023-09-14 05:13:03 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
