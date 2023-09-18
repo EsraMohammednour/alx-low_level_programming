4c63d7da3896dcce39cdf13696313ca0ffe6a3a4
esramohammednour <esraa2023112@gmail.com>
2023-09-18 08:28:22 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap between two numbers
+ *
+ * @a: Integer pointer
+ *
+ * @b: Integer pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
