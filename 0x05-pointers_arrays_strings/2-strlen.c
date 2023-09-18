ad31acabfb4e387a95f69aca3c04c8fa578deff3
esramohammednour <esraa2023112@gmail.com>
2023-09-18 09:56:35 -0700

    Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,6 +4,17 @@
  * _strlen - Length of a string
  *
  * @s: integer pointer
- * Return : s
  *
- *
+ * Return : c
+ */
+ 
+int _strlen(char *s)
+{
+	int c = 0;
+	while (*s !='\n')
+	{
+		c++;
+		s++;
+	}
+	return c;
+}
