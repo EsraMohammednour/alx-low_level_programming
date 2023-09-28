3ade4fe6f8612f86128b3618bb641374b4d73304
esramohammednour <esraa2023112@gmail.com>
2023-09-28 12:13:18 -0700

    Done

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prime1 - Calculate if it is prime or not
+ * @a: variable
+ * @b: Divisor
+ * Return: (0)
+ */
+
+int prime1(int a, int b)
+{
+	if (a < 0 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime1(a, b + 1));
+}
+/**
+ * is_prime_number - checkif it is prime or not
+ * @n: variabl
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	return (prime1(n, 2));
+}
