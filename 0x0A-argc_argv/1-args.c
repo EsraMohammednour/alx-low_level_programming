ee52ec027b7ed7613230830172437594cdbcd501
esraa <esraa2023112@gmail.com>
2023-10-27 19:15:48 +0200

    lib

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: An array containing the program command line arguments
  * Return: 0
  */
-int main(int argc , char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
