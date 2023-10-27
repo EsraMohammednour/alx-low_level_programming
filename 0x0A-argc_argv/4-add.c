219adce03e9109a8fd468ef9522e3932f88d361c
esraa <esraa2023112@gmail.com>
2023-10-27 20:13:54 +0200

    lib

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry main
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int s = 0, i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+		s += atoi(argv[i]);
+	}
+	printf("%d\n", s);
+	return (0);
+}
