31e6adc4d2f602fc3976d5a83e2f1f6ae76167d3
esraa <esraa2023112@gmail.com>
2023-10-31 21:27:14 +0200

    done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * argstostr - Function that concatenates all the arguments
+ * @ac: arguments
+ * @av: char array
+ * Return: char value
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, l = 0;
+	int n = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+		return (0);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+	len += ac;
+	}
+	p = malloc(sizeof(char) * len + 1);
+	if (p == 0)
+	{
+		return (NULL);
+		return (0);
+	}
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j] != '\0'; j++)
+	{
+		p[n] = av[i][j];
+		n++;
+	}
+	if (p[n] == '\0')
+	{
+		p[n++] = '\n';
+	}
+	}
+	return (p);
+}
