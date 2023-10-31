611c32ac0db411ab8533cff01f4384ecdd111511
esraa <esraa2023112@gmail.com>
2023-10-31 20:16:21 +0200

    100-change

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: int array
+ * @height: int value
+ */
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	for(j = 0; j < height; j++)
+	{
+	free(grid[j]);
+	}
+	free(grid);
+}	
