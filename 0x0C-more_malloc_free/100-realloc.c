fea4b163a55afdde3f7d917735c906c56e94a0a9
esraa <esraa2023112@gmail.com>
2023-11-02 14:13:22 +0200

    done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,9 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	p = malloc(new_size);
 	return (p);
-	else if (new_size == old_size)
+	if (new_size == old_size)
 	return (ptr);
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	free(ptr);
 	return (NULL);
 	p = malloc(new_size);
