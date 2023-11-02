6b723d8ab7cc228ce759dc0b7558867fffe1ccec
esraa <esraa2023112@gmail.com>
2023-11-02 14:05:00 +0200

    done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _realloc - function that reallocates a memory
+ *	block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: int value for the old size
+ * @new_size: int value for the new size
+ * Return: char pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *pt = ptr;
+	size_t i;
+
+	if (ptr == NULL)
+	return (malloc(new_size));
+	if (new_size == old_size)
+	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	free(ptr);
+	return (NULL);
+	p = malloc(new_size);
+	if (p == NULL)
+	return (NULL);
+	if (new_size > old_size)
+	new_size = old_size;
+	for (i = 0; i < new_size; i++)
+	p[i] = pt[i];
+	free(ptr);
+	return (p);
+}
