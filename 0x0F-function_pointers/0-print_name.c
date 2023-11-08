61361defdf020299c35978555f106b46f21d7fad
esraa <esraa2023112@gmail.com>
2023-11-08 11:40:14 +0200

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * print_name - function that prints a name
+ * @name: variable 1
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
