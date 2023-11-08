5f7fc07c32ede55af5868c98c645395c63a66ad1
esraa <esraa2023112@gmail.com>
2023-11-08 13:11:05 +0200

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -  Function that searches for an integer
+ * @array: Array of integer
+ * @size: size of array
+ * @cmp: pionter
+ * Return: int value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+	return (-1);
+}
