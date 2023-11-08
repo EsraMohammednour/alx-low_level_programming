25fbd4ea5912e1ba0435e6202bf0db5b1b4ff5ac
esraa <esraa2023112@gmail.com>
2023-11-08 11:59:41 +0200

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
  * print_name - function that prints a name
  * @name: variable 1
- * @*f: pointer to function
+ * @f: pointer to function
  * Return: void
  */
 
