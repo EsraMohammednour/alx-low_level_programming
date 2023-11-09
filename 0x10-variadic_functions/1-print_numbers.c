5f01c6bd0e6bc9bdd48e70fc134c2c1af5bddcad
esraa <esraa2023112@gmail.com>
2023-11-09 13:06:58 +0200

    Done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,8 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list w;
 
 	va_start(w, n);
-	if (separator == NULL)
-	return;
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(w, unsigned int);
