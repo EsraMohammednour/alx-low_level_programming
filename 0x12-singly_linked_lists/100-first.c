29257755efc1879b9b9c2e395afea5018df7b4dd
esra <esraa2023112@gmail.com>
2023-11-16 22:53:43 +0200

    done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * msg - prints a specific message
  * Return: void
  */
-void story(void)
+void msg(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
