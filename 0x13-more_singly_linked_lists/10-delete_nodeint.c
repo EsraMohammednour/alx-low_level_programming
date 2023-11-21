5fa4575c2cffae2a923c2b1ca21df26eff02f11c
esra <esraa2023112@gmail.com>
2023-11-21 14:11:49 +0200

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	for (x = 0; x < idx - 1; x++)
 	{
-		if (esra == NULL || esra->next == NULL)
+		if (!esra || !esra->next)
 		return (-1);
 		esra = esra->next;
 	}
