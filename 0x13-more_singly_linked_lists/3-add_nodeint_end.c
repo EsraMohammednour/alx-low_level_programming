de2dd83f736f84fb51d92827cbf112f1f918cdae
esra <esraa2023112@gmail.com>
2023-11-21 10:52:58 +0200

    done

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Function that adds a new node
+ *	at the end of a listint_t list
+ * @head: Pointer
+ * @n: Int value
+ * Return: Pointer
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *esra, *x;
+
+	esra = malloc(sizeof(listint_t));
+	if (esra == NULL)
+	return (NULL);
+	esra->n = n;
+	esra->next = NULL;
+	if (*head == NULL)
+	{
+		esra->next == *head;
+		return (esra);
+	}
+	x = *head;
+	while (x->next != NULL)
+	x = x->next;
+	x->next = esra;
+	return (esra);
+}
