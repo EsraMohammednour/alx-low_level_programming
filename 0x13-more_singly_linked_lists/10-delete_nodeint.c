4f0e54da665fad5e731deece8d32aa3990636dc0
esra <esraa2023112@gmail.com>
2023-11-21 14:08:59 +0200

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int x;
 	listint_t *esra, *hind;
 
-	if (*head == NULL  || head == NULL)
+	if (*head == NULL || head == NULL)
 	return (-1);
 	esra = *head;
 	if (index == 0)
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	for (x = 0; x < idx - 1; x++)
 	{
-		if (esra != NULL  || esra->next != NULL)
+		if (esra == NULL  || esra->next == NULL)
 		return (-1);
 		esra = esra->next;
 	}
