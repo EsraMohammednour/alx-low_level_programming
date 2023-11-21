22ef50952f2cf27ca8c41bb0b13420c4fcd6ca1a
esra <esraa2023112@gmail.com>
2023-11-21 14:01:55 +0200

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,3 +1,35 @@
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - Function that deletes
+ *	the node at index index of a listint_t linked list
+ * @head: Pointer
+ * @index: int value
+ * Return: int value
+ */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int x;
+	listint_t *esra, *hind;
+
+	if (*head == NULL  || head == NULL)
+	return (-1);
+	esra = *head;
+	if (index == 0)
+	{
+		*head = esra->next;
+		free(esra);
+		return (1);
+	}
+	for (x = 0; x < idx - 1; x++)
+	{
+		if (esra != NULL  || esra->next != NULL)
+		return (-1);
+		esra = esra->next;
+	}
+	hind = esra->next;
+	esra->next = hind->next;
+	free(hind);
+	return (1);
+}
