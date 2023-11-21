1a1ef3c0d25925425d9a6d0734b66399749c3ddf
esra <esraa2023112@gmail.com>
2023-11-21 16:42:56 +0200

    done

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#iclude "lists.h"
+
+/**
+ * find_listint_loop - Function that finds the loop in a linked list
+ * @head: Pointer
+ * Return: pointer
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *h, *t;
+	if (!head)
+	return (NULL);
+	h = t = head;
+	while (t && h && h->next)
+	{
+		t = t->next;
+		h = (h->next)->next;
+		if (h == t)
+		{
+			t = head;
+			while (t && h)
+			{
+				if (t == h)
+				return (t);
+				t = t->next;
+				h = h->next;
+			}
+		}
+	}
+
+	return (NULL);
+}
