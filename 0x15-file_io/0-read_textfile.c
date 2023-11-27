d1bf84611b3c114feb28d4fc9358494537c669e1
esra <esraa2023112@gmail.com>
2023-11-27 12:32:46 +0200

    Done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	f = open(filename, O_RDONLY);
 	if (f == -1)
 	return (0);
-	x = read(of, &buffer[], letters);
+	x = read(f, &buffer[], letters);
 	x = write(STDOUT_FILENO, &buffer[], x);
 
 	close(f);
