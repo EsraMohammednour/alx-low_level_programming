43627246e5b5bfe80ee8ce30e4f08aba91191f08
esra <esraa2023112@gmail.com>
2023-11-27 13:30:35 +0200

    Done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,15 +8,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int f, b = 0;
+	int l = 0, f, b = 0;
 
+	l = strlen(text_content);
 	if (!filename)
 		return (-1);
 	f = open(filename, O_WRONLY | O_APPEND);
 	if (f == -1)
 	return (-1);
 	if (strlen(text_content))
-	bytes = write(f, text_content, strlen(text_content));
+	b = write(f, text_content, l);
 	close(f);
-	return (b == strlen(text_content) ? 1 : -1);
+	return (b == l ? 1 : -1);
 }
