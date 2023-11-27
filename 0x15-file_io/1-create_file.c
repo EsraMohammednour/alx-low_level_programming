487d10e722a52360a66f00dea339c30c4e657dec
esra <esraa2023112@gmail.com>
2023-11-27 12:51:05 +0200

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
-
+/**
+ * create_file - Function that creates a file
+ * @filename: The name of the file
+ * @text_content: Char value
+ * Return: int value
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int f, w;
+	int f, w x;
 
+	x = strlen(text_content);
 	if (filename == NULL)
 	return (0);
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -12,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 	if (text_content)
 	{
-		w = write(fd, text_content, strlen(text_content));
+		w = write(f, text_content, x);
 		if (w == -1)
 		return (-1);
 	{
