e5cad672d1aa8040532f95b85d08bf26b0cc2142
esra <esraa2023112@gmail.com>
2023-11-27 12:53:56 +0200

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int f, w x = 0;
 
-	x = strlen(text_content);
 	if (filename == NULL)
 	return (0);
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -18,6 +17,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 	if (text_content)
 	{
+		x = strlen(text_content);
 		w = write(f, text_content, x);
 		if (w == -1)
 		return (-1);
