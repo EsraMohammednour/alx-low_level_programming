5eb893f3ccb9590bdb4db4eba2c802f7fb7d957a
esra <esraa2023112@gmail.com>
2023-11-27 12:26:52 +0200

    Done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#define BUFFER_SIZE 1024
+
+/**
+ * read_textfile - Function that reads a text file and prints
+ *	it to the POSIX standard output
+ * @filename:It is tne name of the file
+ * @letters: unsigned int value
+ * Return: int value
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	char buffer[BUFFER_SIZE * 8];
+	ssize_t x;
+
+	if (filename == NULL || !letters)
+	return (0);
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+	return (0);
+	x = read(of, &buffer, letters);
+	x = write(STDOUT_FILENO, &buffer, p);
+
+	close(f);
+	return (p);
+}
