55b9dc621d03321dbb5960ccc935c4981526b8af
esraa <esraa2023112@gmail.com>
2024-01-11 22:16:46 +0200

    Done

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,4 +27,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	last = *head;
-
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+	return (new);
+}
