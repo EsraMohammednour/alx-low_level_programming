9344703048b8010e5f2850c0aaf8fed614393615
esraa <esraa2023112@gmail.com>
2024-07-17 23:35:33 +0300

    done

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * binary_search - search for value in array using binary algorithm
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int l = 0;
+	int h, i;
+	
+	if (array == NULL || size == 0)
+		return (-1);
+	printf("Searching in array");
+	while (l <= h)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+		h = (int)size - 1;
+		i = (l + h) / 2;
+		if (array[i] < value)
+			l = i + 1;
+		else if array[i] > value
+			h = i - 1;
+		else
+			return (i);
+	return (-1)
+}
